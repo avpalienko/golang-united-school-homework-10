c9722a055df31de99c3bb5d9cfcf7326692a37ae
Alexey Palienko <a.v.palienko@gmail.com>
2022-06-27 23:47:14 +0300

    1

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"os"
 	"strconv"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -19,10 +22,28 @@ main function reads host/port from env just for an example, flavor it following 
 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
-	router := mux.NewRouter()
+	mux := mux.NewRouter()
+	//mux := http.NewServeMux()
+
+	reqCtx := &reqContext{verbose: true}
+
+	mux.Handle("/bad", handlers.LoggingHandler(os.Stdout,
+		Handler(reqCtx, "GET", http.HandlerFunc(getBad)))).Methods("GET")
+
+	mux.Handle("/name/{param}", handlers.LoggingHandler(os.Stdout,
+		Handler(reqCtx, "GET", http.HandlerFunc(getName))))
+
+	mux.Handle("/data", handlers.LoggingHandler(os.Stdout,
+		Handler(reqCtx, "POST", http.HandlerFunc(getData)))).Methods("POST")
+
+	mux.Handle("/headers", handlers.LoggingHandler(os.Stdout,
+		Handler(reqCtx, "POST", http.HandlerFunc(getHeaders))))
+
+	mux.Handle("/get-echo", handlers.LoggingHandler(os.Stdout,
+		Handler(reqCtx, "GET", http.HandlerFunc(getEcho))))
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
-	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), mux); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -35,4 +56,102 @@ func main() {
 		port = 8081
 	}
 	Start(host, port)
+}
+
+// getEcho Retrieves the echo response
+func getEcho(w http.ResponseWriter, r *http.Request) {
+	log.Println("Echoing back request made to " + r.URL.Path + " to client (" + r.RemoteAddr + ")")
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// allow pre-flight headers
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
+
+	err := r.Write(w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getBad(w http.ResponseWriter, r *http.Request) {
+	httpError(w, http.StatusInternalServerError) // 500
+}
+
+func getName(w http.ResponseWriter, r *http.Request) {
+	//urlTokens := strings.Split(r.RequestURI, "/")
+	//param := fmt.Sprintf("Hello, %s!", urlTokens[len(urlTokens)-1])
+	vars := mux.Vars(r)
+	param := fmt.Sprintf("Hello, %s!", vars["param"])
+	_, err := w.Write([]byte(param))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	param := fmt.Sprintf("I got message:\n%s", string(body))
+	_, err = w.Write([]byte(param))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func getHeaders(w http.ResponseWriter, r *http.Request) {
+	a := r.Header["a"]
+	b := r.Header["b"]
+	ai, err := strconv.Atoi(a[0])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	bi, err := strconv.Atoi(b[0])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("a+b", strconv.Itoa(ai+bi))
+}
+
+func Handler(reqCtx *reqContext, method string, h http.Handler) http.Handler {
+	return handler{reqCtx, method, h}
+}
+
+type reqContext struct {
+	verbose bool
+}
+
+type handler struct {
+	reqCtx  *reqContext
+	method  string
+	handler http.Handler
+}
+
+func (vh handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if vh.reqCtx.verbose {
+		reqDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		log.Printf("\n%s", reqDump)
+	}
+	if r.Method != vh.method {
+		httpError(w, http.StatusMethodNotAllowed) // 405
+		return
+	}
+	vh.handler.ServeHTTP(w, r)
+}
+
+func httpError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
